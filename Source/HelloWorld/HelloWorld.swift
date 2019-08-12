e52529cf274362205442d0ad9522f70c936efdfd
Saleem Abdulrasool <compnerd@compnerd.org>
2019-08-11 22:16:14 -0700

    add an example of module specific language version

--- Source/HelloWorld/HelloWorld.swift
+++ Source/HelloWorld/HelloWorld.swift
@@ -1,5 +1,24 @@
 
 import HelloWorldCore
 
-print(HelloWorldCore.string)
+struct HelloWorldStringAccessor {
+  func access() throws -> String? {
+    return HelloWorldCore.string
+  }
+}
+
+func display(_ message: String) -> Bool {
+  print(message)
+  return true
+}
+
+func main() {
+  let accessor: HelloWorldStringAccessor = HelloWorldStringAccessor()
+  guard let message = try? accessor.access(), display(message!) else {
+    print("unable to display message")
+    return
+  }
+}
+
+main()
 
