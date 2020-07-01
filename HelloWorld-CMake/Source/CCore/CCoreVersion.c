e93b191f990d28235c1192e626e8232c9a160e45
Saleem Abdulrasool <compnerd@compnerd.org>
2020-06-30 21:02:43 -0700

    CCore: add a header, new apis, and modulemap

--- HelloWorld-CMake/Source/CCore/CCoreVersion.c
+++ HelloWorld-CMake/Source/CCore/CCoreVersion.c
@@ -1,19 +1,15 @@
 
-#if defined(__ELF__) || defined(__MACH__) || defined(__WASM__)
-  #if defined(CCore_EXPORTS)
-    #define CCORE_ABI __attribute__((__visibility__("default")))
-  #else
-    #define CCORE_ABI __attribute__((__visibility__("default")))
-  #endif
-#else // assume PE/COFF
-  #if defined(CCore_EXPORTS)
-    #define CCORE_ABI __declspec(dllexport)
-  #else
-    #define CCORE_ABI __declspec(dllimport)
-  #endif
-#endif
+#include "CCoreVersion.h"
 
 int CCORE_ABI CGetVersionMajor(void) {
   return 1;
+}
+
+int CCORE_ABI CGetVersionMinor(void) {
+  return 0;
+}
+
+int CCORE_ABI CGetVersionPatch(void) {
+  return 0;
 }
 
---
+++ HelloWorld-CMake/Source/CCore/CCoreVersion.h
@@ -0,0 +1,11 @@
+
+#ifndef CCore_Version_h
+#define CCore_Version_h
+
+#include "CCoreMacros.h"
+
+int CCORE_ABI CGetVersionMajor(void);
+int CCORE_ABI CGetVersionMinor(void);
+int CCORE_ABI CGetVersionPatch(void);
+
+#endif
