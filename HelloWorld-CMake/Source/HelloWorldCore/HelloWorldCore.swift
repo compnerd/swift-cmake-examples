48acb389ac7ddd8f09309cfc1a7c6feb1a9a4ac1
Saleem Abdulrasool <compnerd@compnerd.org>
2020-06-30 21:07:06 -0700

    HelloWorldCore: add a dependency on a C library
    This adds a dependency on a C library to demonstrate how to integrate a
    C library into a Swift library.

--- HelloWorld-CMake/Source/HelloWorldCore/HelloWorldCore.swift
+++ HelloWorld-CMake/Source/HelloWorldCore/HelloWorldCore.swift
@@ -1,7 +1,14 @@
 
+@_implementationOnly
+import CCore
+
 public struct HelloWorldCore {
   public static var string: String {
     return _HelloWorldCore.string
+  }
+
+  public static var version: String {
+    return "\(CGetVersionMajor()).\(CGetVersionMinor()).\(CGetVersionPatch())"
   }
 }
 
